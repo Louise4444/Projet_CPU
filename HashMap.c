4c1010c48b5df714ffc77914aef86c7537fcb814
Louise Azem <louiseazem4444@gmail.com>
2025-03-10 13:33:49 +0100

    modifications

---
+++ HashMap.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "HashMap.h"
+
+
+unsigned long simple_hash(const char *str){
+
+    return 0;
+}
+
+HashMap* hashmap_create(){
+
+
+}
+
+
+int hashmap_insert(HashMap *map, const char *key, void *value){
+
+
+    return 0;
+
+}
+
+
+void* hashmap_get(HashMap *map, const char *key){
+
+
+}
+
+
+int hashmap_remove(HashMap *map, const char *key){
+
+
+    return 0;
+}
+
+
+void hashmap_destroy(HashMap *map){
+
+
+}
