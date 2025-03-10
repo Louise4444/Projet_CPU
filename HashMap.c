b1d4306cbba42feb36c008210be77f774c63dc8e
Louise Azem <louiseazem4444@gmail.com>
2025-03-10 15:47:08 +0100

    ajout des fonctions simple_hash hashmap_crate et hashmap_insert

--- HashMap.c
+++ HashMap.c
@@ -3,21 +3,56 @@
 #include "HashMap.h"
 
 
+// convertir une chaı̂ne de caractères en un indice dans la table de hachage.
 unsigned long simple_hash(const char *str){
 
-    return 0;
+    unsigned long converted_str;
+
+    for(int i=0; str[i]!= '\0'; i++){
+        converted_str+= str[i];
+    }
+    
+    return converted_str;
 }
 
 HashMap* hashmap_create(){
 
+    HashMap* hash = malloc(sizeof(HashMap));
+
+    if (!hash){
+        fprintf(stderr,"Erreur d'allocation de la table de hachage %s %s %d \n", __FILE__, __PRETTY_FUNCTION__, __LINE__);
+    }
+
+    hash->size=127;
+    hash->table= malloc(sizeof(HashEntry)*127); 
+
+    if (!hash->table){
+        fprintf(stderr,"Erreur d'allocation de la table de hachage %s %s %d \n", __FILE__, __PRETTY_FUNCTION__, __LINE__);
+    }
+
+    for(int i =0; i<hash->size; i++){
+        hash -> table[i].key = NULL;
+        hash -> table[i].value = NULL;
+    }
+
+    return hash;
 
 }
 
 
 int hashmap_insert(HashMap *map, const char *key, void *value){
 
+    if (!map || !(map->table)){
+        fprintf (stderr,"table de hachage non existente %s %s %d \n", __FILE__, __PRETTY_FUNCTION__, __LINE__);
+        exit(64);
+    }
 
-    return 0;
+    int indice_insert= simple_hash(key);
+
+    map->table[indice_insert].key=key;
+    map->table[indice_insert].value=value;
+
+    return indice_insert;
 
 }
 
